5fa255b7d9fe4ef09d1bd2bb1d6a93e832aa7c84
xy666 <xy850208@163.com>
2023-05-15 22:00:06 +0800

    modify dltfx for add ssq random choice func

--- dltfx.py
+++ dltfx.py
@@ -1,4 +1,6 @@
 import random
+import json
+import sys
 
 import xlrd, xlwt
 
@@ -11,31 +13,128 @@ oshu = [2,8,10,22,24]
 tongwei = [[1,11,21],[2,22],[5,25],[9,19]]
 lianhao = [[1,2],[7,8,9,10,11],[21,22],[24,25]]
 
-#data = xlrd.open_workbook(r"E:\workspace\code\piao\data\1112.xlsx")
+redAll = set(range(1, 34))
+zhishu = {1,2,3,5,7,11,13,17,19,23,29,31}
+heshu = redAll - zhishu
 
-# table = data.sheets()[0]
-# print(table.nrows,table.ncols)
-# rows = table.get_rows()
-# for row in rows:
-#     print(row[1])
+xiaoshu = set(range(1, 17))
+dashu = set(range(18, 34))
+
+data = xlrd.open_workbook(r"E:\workspace\code\piao\data\ssq_origin_data.xlsx")
+
+table = data.sheets()[0]
+#print(table.nrows, table.ncols)
+rows = table.get_rows()
+def getLatestN(n):
+    set_latest5 = {1} & {2}
+    for m in range(n):
+        cellValue_str = table.row(m+1)[2].value
+        #print(fr'cellValue_str: {cellValue_str}')
+        cellValue_set = set(int(item) for item in cellValue_str.split(' '))    #将列表中str元素转成int再将整个列表转成set
+
+        set_latest5 = set_latest5 | cellValue_set     #近5合集
+        # print(set_latest5)
+        # print(len(set_latest5))
+    print(set_latest5)
+    print(len(set_latest5))
+    return list(set_latest5)
+
+def getHeshuNum(s):
+    n = 0
+    for i in s:
+        if i in list(heshu):
+            n += 1
+    return n
+
+def getXiaoshuNum(s):
+    n = 0
+    for i in s:
+        if i in xiaoshu:
+            n += 1
+    return n
+
+def getOushuNum(s):
+    n = 0
+    for i in s:
+        if i % 2 == 0:
+            n += 1
+    return n
+
+def get2LianNum(s):
+    n = 0
+    s.sort()
+    for i in range(len(s) - 1):
+        if abs(s[i] - s[i + 1]) == 1:
+            n += 1
+    return n
+
+def get3LianNum(s):
+    n = 0
+    s.sort()
+    for i in range(len(s) - 2):
+        if abs(s[i] - s[i + 1]) == 1 and abs(s[i + 1] - s[i + 2]) == 1:
+            n += 1
+    return n
+
+def getUnitSameNum(s):
+    n = 0
+    dic = {}
+    for i in s:
+        unit = i % 10
+        if unit in dic.keys():
+            dic[unit] += 1
+        else:
+            dic[unit] = 1
+    # print(dic)
+    for value in dic.values():
+        if value == 2 or value == 3:
+            n += 1
+    return n
 
 
-def rangeball(num):
-    for n in range(num):
-        allRed = list(range(1,34))
-        allBlue = list(range(1,17))
-        redDone = []
 
-        # choice redball
-        for i in range(1, 7):
-            redTemp = random.choice(allRed)
-            redDone.append(redTemp)
-            allRed.remove(redTemp)
+def rangeball(num, houxuan):
+    n = 0
+    allBlue = list(range(1, 17))
+    # choice blueball
+    blueDone = random.choice(allBlue)
+    while 1:
+        if houxuan:
+            red_houxuan = houxuan
+        else:
+            red_houxuan = [1,2,5,7,8,9,10,11,19,21,22,24,25]
 
-        # choice blueball
-        blueDone = random.choice(allBlue)
-        # choice result
-        print(f"{sorted(redDone)}   {blueDone}")
+        if n < num:
+            # choice redball
+            redDone1 = random.sample(red_houxuan, 4)
+            #for i in range(1, 7):
+                # redTemp = random.choice(allRed)
+                #redTemp = random.choice(red_houxuan)
+                #redDone.append(redTemp)
+                #red_houxuan.remove(redTemp)
+            redDone2 = random.sample(list(redAll - set(red_houxuan)), 2)
+            #redDone = set(redDone1) | set(float(i) for i in redDone2)
+            redDone = set(redDone1) | set(redDone2)
+
+            ou = getOushuNum(redDone)
+            xiao = getXiaoshuNum(redDone)
+            he = getHeshuNum(redDone)
+            lian_2 = get2LianNum(list(redDone))
+            lian_3 = get3LianNum(list(redDone))
+            tong = getUnitSameNum(redDone)
+
+            if ou in range(2, 5) and xiao in range(2, 5) and he in range(3, 6) and lian_2 in range(0, 3) and \
+                    lian_3 <= 1 and tong in [1, 2]:
+                # choice result
+                print(fr"偶: {ou}  小：{xiao}  合：{he}  2连：{lian_2}  3连：{lian_3}  同尾：{tong}组,  {' '.join(str(i) for i in sorted(redDone))} + {blueDone}")
+                n += 1
+            else:
+                continue
+        else:
+            print('CHOICE OVER!')
+            break
 
 if __name__ == "__main__":
-    rangeball(5)
+    Latest5 = getLatestN(5)
+    rangeball(10, Latest5)
+    #getUnitSameNum([1,5,12,15,19,29])
--- gpxx.py
+++ gpxx.py
@@ -9,20 +9,20 @@ codes = ["sz000665","sz000702","sz000756","sz002031","sz002194","sz002415","sz00
 #0: 未知 1: 名字 2: 代码 3: 当前价格 4: 昨收 5: 今开 6: 成交量（手）7: 外盘 8: 内盘 9: 买一 10: 买一量（手）11-18: 买二 买五 19: 卖一 20: 卖一量 21-28: 卖二 卖五 29: 最近逐笔成交 30: 时间 31: 涨跌 32: 涨跌%
 #33: 最高 34: 最低 35: 价格/成交量（手）/成交额 36: 成交量（手）37: 成交额（万）38: 换手率 39: 市盈率 40: 41: 最高 42: 最低 43: 振幅 44: 流通市值 45: 总市值 46: 市净率 47: 涨停价 48: 跌停价
 
-table = PrettyTable(['名称','实价','昨收','今开','涨跌(元)','涨跌幅','最高','最低','成交额(万)','换手率','时间','涨停价','跌停价'])
+table = PrettyTable(['名称','实价','昨收','今开','涨跌(元)','涨跌幅%','最高','最低','成交额(万)','换手率','时间','涨停价','跌停价'])
 init(autoreset=False)
 
 for code in codes:
     result = requests.get(api_url+code)
     result_array = result.text.split('~')
-    if float(result_array[32]) >= 5:
+    if float(result_array[32]) >= 3:
         result_array[32] = Style.BRIGHT + Fore.LIGHTRED_EX + result_array[32] + Fore.RESET
         #result_array[32] = "\033[0;31;40m" + result_array[32] + "\033[0m"
-    elif float(result_array[32]) <= -5:
+    elif float(result_array[32]) <= -3:
         result_array[32] = Style.BRIGHT + Fore.GREEN + result_array[32] + Fore.RESET
         #result_array[32] = "\033[0;32m" + result_array[32] + "\033[0m"
 
-    table.add_row([result_array[1],result_array[3],result_array[4],result_array[5],result_array[31],result_array[32],result_array[33],result_array[34],result_array[37],result_array[38],result_array[30][4:8]+'-'+result_array[30][8:],result_array[47],result_array[48]])
+    table.add_row([result_array[1],result_array[3],result_array[4],result_array[5],result_array[31],result_array[32]+'%',result_array[33],result_array[34],result_array[37],result_array[38],result_array[30][4:8]+'-'+result_array[30][8:],result_array[47],result_array[48]])
     time.sleep(1)
 
 print(table)
